bc233d7d254c7017098a00a9dc893d0c31ad0240
senner007 <nielshtg@hotmail.com>
2018-04-05 14:40:20 +0200

    Add project files.

---
+++ Sales_ForeCast_Improved/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Sales_ForeCast_Improved
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ticketsSold = new System.Windows.Forms.TextBox();
+            this.tvCover = new System.Windows.Forms.TextBox();
+            this.sportsVisitors = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ticketsSold
+            // 
+            this.ticketsSold.Location = new System.Drawing.Point(103, 34);
+            this.ticketsSold.Name = "ticketsSold";
+            this.ticketsSold.Size = new System.Drawing.Size(100, 20);
+            this.ticketsSold.TabIndex = 0;
+            this.ticketsSold.Tag = "ticketsSold";
+            // 
+            // tvCover
+            // 
+            this.tvCover.Location = new System.Drawing.Point(103, 91);
+            this.tvCover.Name = "tvCover";
+            this.tvCover.Size = new System.Drawing.Size(100, 20);
+            this.tvCover.TabIndex = 1;
+            this.tvCover.Tag = "tvCover";
+            // 
+            // sportsVisitors
+            // 
+            this.sportsVisitors.Location = new System.Drawing.Point(103, 143);
+            this.sportsVisitors.Name = "sportsVisitors";
+            this.sportsVisitors.Size = new System.Drawing.Size(100, 20);
+            this.sportsVisitors.TabIndex = 2;
+            this.sportsVisitors.Tag = "sportsVisitors";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Tag = "errorticketsSold";
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Tag = "errortvCover";
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Tag = "errorsportsVisitors";
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(553, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(553, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Tag = "calender";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sportsVisitors);
+            this.Controls.Add(this.tvCover);
+            this.Controls.Add(this.ticketsSold);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ticketsSold;
+        private System.Windows.Forms.TextBox tvCover;
+        private System.Windows.Forms.TextBox sportsVisitors;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sales_ForeCast_Improved/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Sales_ForeCast_Improved
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        int Validate(string s)
+        {
+            return Int32.TryParse(s, out int parsed) && parsed > -1 || s == "" ? parsed : -1;
+        }
+        List<int> ValidateTextboxes (params object[] list)
+        {
+            var returnList = new List<int>();
+            foreach (string tbTag in list)
+            {
+                int validated = Validate(this.Controls.OfType<TextBox>().FirstOrDefault(t => (string)t.Tag == tbTag).Text);
+                if (validated == -1)
+                {
+                    ToggleErrorLabel(tbTag, true);              
+                }   
+                else
+                {
+                    ToggleErrorLabel(tbTag, false);
+                    returnList.Add(validated);
+                }
+            }
+            return returnList.Count() == list.Count() ? returnList : new List<int>();
+        }
+
+        void ToggleErrorLabel (string s, bool onOff) => this.Controls.OfType<Label>().FirstOrDefault(w => (string)w.Tag == "error" + s).Visible = onOff;
+     //  void ClearErrorLabels() => this.Controls.OfType<Label>().Where(l => l.Tag.ToString().StartsWith("error")).ToList().ForEach(l => l.Visible = false);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var validated = ValidateTextboxes(
+                   this.ticketsSold.Tag,
+                   this.tvCover.Tag,
+                   this.sportsVisitors.Tag
+                );
+
+            if (validated.Count() != 0)
+            {
+              //  Console.WriteLine(Convert.ToDateTime(dateTimePicker1.Value));
+                // dateTimePicker skal valideres først
+                SalesForecastPlural.list.Add(
+                    Convert.ToDateTime(dateTimePicker1.Value).ToShortDateString() + " " + DateTime.Now.ToString("hh.mm.ss.ff"), 
+                    new SalesForecast(validated[0], validated[1], validated[2] )
+                    );
+
+                Console.WriteLine(string.Join("", SalesForecastPlural
+                    .list.Select(x => "\n - Date: " + x.Key +
+                    " \n - Forecast: " +
+                    " Tickets Sold: " + x.Value.TicketsSold +
+                    ", TV Cover: " + x.Value.TVCover +
+                    ", Sports Visitors: " + x.Value.SportsVisitors).ToArray())
+                 );
+                   
+
+            }
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            this.textBox1.Text = Convert.ToDateTime(dateTimePicker1.Value).ToShortDateString();
+        }
+    }
+    class SalesForecast
+    {
+        public int TicketsSold { get; private set; }
+        public int TVCover { get; private set; }
+        public int SportsVisitors { get; private set; }
+        public SalesForecast(int ticketsSold, int tvCover, int sportsVisitors)
+        {
+            TicketsSold = ticketsSold;
+            TVCover = tvCover;
+            SportsVisitors = sportsVisitors;
+        }
+    }
+    static class SalesForecastPlural
+    {
+        public static Dictionary<string, SalesForecast> list = new Dictionary<string, SalesForecast>();
+    }
+
+}
+
+
